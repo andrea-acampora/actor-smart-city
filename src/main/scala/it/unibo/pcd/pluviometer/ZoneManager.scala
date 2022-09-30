de882b8faddab43c4e2d05cb0dc38b9a32e14ca9
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-09-30 15:11:41 +0200

    fix: remove wrong indentation

--- src/main/scala/it/unibo/pcd/pluviometer/ZoneManager.scala
+++ src/main/scala/it/unibo/pcd/pluviometer/ZoneManager.scala
@@ -26,21 +26,27 @@ object ZoneManager:
   ): Behavior[Command | Receptionist.Listing] =
     Behaviors.receiveMessage {
       case msg: Receptionist.Listing =>
-        val pluoviometersList: List[ActorRef[Command]] = msg.serviceInstances(ServiceKey[Command](serviceKey)).toList
-        if (pluoviometersList == pluviometers) Behaviors.same
-        else zoneManagerLogic(ctx, serviceKey, fireStation, pluoviometersList)
+        val pluviometersList: List[ActorRef[Command]] = msg.serviceInstances(ServiceKey[Command](serviceKey)).toList
+        if (pluviometersList == pluviometers)
+          Behaviors.same
+        else
+          zoneManagerLogic(ctx, serviceKey, fireStation, pluviometersList)
       case NotifyAlarm() => manageAlarmBehaviour(ctx, serviceKey, fireStation, pluviometers)
+      case _ => Behaviors.same
     }
 
-    def manageAlarmBehaviour(
-        ctx: ActorContext[Command | Receptionist.Listing],
-        serviceKey: String,
-        fireStation: ActorRef[Command],
-        pluviometers: List[ActorRef[Command]]
-    ): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
-      case msg: Receptionist.Listing =>
-        val pluoviometersList: List[ActorRef[Command]] = msg.serviceInstances(ServiceKey[Command](serviceKey)).toList
-        if (pluoviometersList == pluviometers) Behaviors.same
-        else manageAlarmBehaviour(ctx, serviceKey, fireStation, pluoviometersList)
-      case NotifyState(state) => Behaviors.same
-    }
+  def manageAlarmBehaviour(
+      ctx: ActorContext[Command | Receptionist.Listing],
+      serviceKey: String,
+      fireStation: ActorRef[Command],
+      pluviometers: List[ActorRef[Command]]
+  ): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
+    case msg: Receptionist.Listing =>
+      val pluviometersList: List[ActorRef[Command]] = msg.serviceInstances(ServiceKey[Command](serviceKey)).toList
+      if (pluviometersList == pluviometers)
+        Behaviors.same
+      else
+        manageAlarmBehaviour(ctx, serviceKey, fireStation, pluviometersList)
+    case NotifyState(state) => Behaviors.same
+    case _ => Behaviors.same
+  }
